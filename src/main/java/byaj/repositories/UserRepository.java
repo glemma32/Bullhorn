57b043e53dd10b5ba4c0595e9420935a6a8bb909
student <student@GBTC441J03LX.montgomerycollege.edu>
2017-07-10 17:52:10 -0400

    fixed search

--- src/main/java/byaj/repositories/UserRepository.java
+++ src/main/java/byaj/repositories/UserRepository.java
@@ -12,6 +12,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     public User findOneByUsername(String num);
     public List<User> findAllByFullNameOrderByIdAsc(String fullName);
     public List<User> findAllByOrderByLastNameAscFirstNameAsc();
+    public List<User> findAllByUsernameOOrderByUserDateDesc(String username);
     User findByEmail(String email);
     int countByEmail(String email);
     int countByUsername(String username);
